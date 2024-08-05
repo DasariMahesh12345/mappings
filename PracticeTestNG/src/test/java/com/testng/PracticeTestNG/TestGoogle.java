d3fc66854a034e680a703281ef9fba520477ea9a
DasariMahesh12345 <131242450+DasariMahesh12345@users.noreply.github.com>
2024-08-05 13:13:50 +0530

    Add files via upload

---
+++ PracticeTestNG/src/test/java/com/testng/PracticeTestNG/TestGoogle.java
@@ -0,0 +1,54 @@
+package com.testng.PracticeTestNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestGoogle {
+	
+	public static WebDriver driver;
+	
+	@Test
+	public void testGoogleChrome() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		 driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//		Thread.sleep(4000);
+//		driver.findElement(By.name("username")).sendKeys("Admin");
+//		Thread.sleep(4000);
+//		driver.findElement(By.name("password")).sendKeys("admin123");
+//		System.out.println(driver.getTitle());
+//		driver.findElement(By.xpath("//button[@type='submit']")).click();
+//		System.out.println(driver.getTitle());
+		Thread.sleep(5000);
+		
+	}
+	@Test
+	public void login() throws InterruptedException {
+		Thread.sleep(4000);
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		Thread.sleep(4000);
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+	
+	
+//	@Test
+//	public void faceBook() throws InterruptedException {
+//		WebDriverManager.chromedriver().setup();
+//		WebDriver driver =new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://www.facebook.com/");
+//		driver.findElement(By.name("email")).sendKeys("mahesh", Keys.ENTER);
+//		System.out.println(driver.getTitle());
+//		Thread.sleep(5000);
+//		driver.quit();
+//	}
+
+
+}
